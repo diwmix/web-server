9aff8145bbc28ada0711d5215a652d2783887427
diwmix <116847359+diwmix@users.noreply.github.com>
2023-03-06 13:11:29 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const hostname = 'localhost';
+const port = 3000;
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+        fs.readFile('index.html', (err, data) => {
+          res.writeHead(200, {'Content-Type': "text/html"})
+          res.end(data)
+        })
+        
+})
+server.listen(port, hostname, ()=>{
+    console.log('running')
+})
